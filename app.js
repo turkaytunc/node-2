a7312f34c1062ac2a0fa2b58aa2efe6dbbb3e622
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-14 21:03:20 +0300

    Added files for better routes

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require("express");
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(3000, "localhost");
