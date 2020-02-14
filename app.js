5ce044e0d83c5dc5b7123cee9c4dae20c5c03dd2
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-14 21:10:52 +0300

    Added 404 page for unhandled routes

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>${res.statusCode} Page not found!!</h1>`);
+});
 app.listen(3000, "localhost");
