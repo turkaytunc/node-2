fb2cbb648ca0bc686fb23f51836a8d2fbe98d842
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-14 22:17:55 +0300

    Added style

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
