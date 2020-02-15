20faf87377c361b40674bbee78efcd492bd6b20f
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-15 21:07:49 +0300

    Update: added template files

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require("express");
 const path = require("path");
 
-const adminRoutes = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
 });
 
-app.listen(3000, "localhost");
+app.listen(3000);
