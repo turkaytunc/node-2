1352c2b9ac104eeed484224ad8cca22ba7d71db5
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-15 13:22:35 +0300

    Update: style files

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404page.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, "localhost");
